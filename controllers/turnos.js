e2f7155f011ef2552532358bda0b332edf1f4435
leonel <leonelpiter@gmail.com>
2025-10-10 09:44:18 -0300

    Copio dashboard, login y controllers desde pruebas_leonel

---
+++ controllers/turnos.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("turnoForm");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const turno = {
+      nombre: document.getElementById("nombre").value.trim(),
+      email: document.getElementById("email").value.trim().toLowerCase(),
+      fecha: document.getElementById("fecha").value,
+      hora: document.getElementById("hora").value,
+      profesional: document.getElementById("profesionales").value
+    };
+
+    // obtengo los turnos desde el storage
+    const turnos = JSON.parse(localStorage.getItem("turnos")) || [];
+
+    // Verifico si ya existe el turno
+    const yaExiste = turnos.some(t =>
+      t.email === turno.email &&
+      t.fecha === turno.fecha &&
+      t.hora === turno.hora
+    );
+
+    if (yaExiste) {
+      alert(" Este turno ya fue reservado con ese email en esa fecha y hora.");
+      return;
+    }
+
+    // Guardar el turno
+    turnos.push(turno);
+    localStorage.setItem("turnos", JSON.stringify(turnos));
+    alert(" Turno reservado con éxito.");
+    form.reset();
+  });
+});
+
