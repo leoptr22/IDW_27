6f69c58460bc5f85fc7095f2cba57b6eaa1fcbdf
leonel <leonelpiter@gmail.com>
2025-11-12 16:53:23 -0300

    add medicos inicio turnos

--- controllers/turnos.js
+++ controllers/turnos.js
@@ -2,14 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("turnoForm");
   const selectProfesionales = document.getElementById("profesionales");
 
-  //  localStorage
   function cargarMedicosSelect() {
-    const medicos = JSON.parse(localStorage.getItem("medicos")) || [];
-    selectProfesionales.innerHTML = ""; 
-
-    medicos.forEach((m, index) => {
+    let medicos = JSON.parse(localStorage.getItem("medicos"));
+    if (!medicos || medicos.length === 0) {
+      medicos = [
+        { nombre: "Dr. Juan Pérez", especialidad: "Cardiologia Infantil" },
+        { nombre: "Dr. Manuel Aboy", especialidad: "Traumatologia" },
+        { nombre: "Dr. Osvaldo Rizzo", especialidad: "Clinica Medica" },
+      ];
+      localStorage.setItem("medicos", JSON.stringify(medicos));
+    }
+    selectProfesionales.innerHTML = "";
+    medicos.forEach((m) => {
       const option = document.createElement("option");
-      option.value = m.nombre; 
+      option.value = m.nombre;
       option.textContent = `${m.nombre} - ${m.especialidad}`;
       selectProfesionales.appendChild(option);
     });
@@ -26,16 +32,13 @@ document.addEventListener("DOMContentLoaded", () => {
       fecha: document.getElementById("fecha").value,
       hora: document.getElementById("hora").value,
       obraSocial: document.getElementById("obra_social").value,
-
-      profesional: selectProfesionales.value
+      profesional: selectProfesionales.value,
     };
 
     const turnos = JSON.parse(localStorage.getItem("turnos")) || [];
 
-    const yaExiste = turnos.some(t =>
-      t.email === turno.email &&
-      t.fecha === turno.fecha &&
-      t.hora === turno.hora
+    const yaExiste = turnos.some(
+      (t) => t.email === turno.email && t.fecha === turno.fecha && t.hora === turno.hora
     );
 
     if (yaExiste) {
@@ -49,4 +52,3 @@ document.addEventListener("DOMContentLoaded", () => {
     form.reset();
   });
 });
-
