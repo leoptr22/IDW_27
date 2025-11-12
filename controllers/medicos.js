9417141826fa7fb271f77526980abf7b16ca4b7b
leonel <leonelpiter@gmail.com>
2025-11-12 17:47:27 -0300

    add medicos inicio turnos

--- controllers/medicos.js
+++ controllers/medicos.js
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
   let medicoSeleccionado = null;
 
   const medicosIniciales = [
-    { nombre: "Dr. Juan Pérez", especialidad: "Cardiología",consulta:"$ 40000", obraSocial: "osde", matricula: "256985", foto: "./fotos/1.jpg" },
-    { nombre: "Dr. Manuel Aboy", especialidad: "Traumatología", consulta:"$ 40000", obraSocial: "Pami", matricula: "254796", foto: "./fotos/2.jpg" },
-    { nombre: "Dr. Osvaldo Rizzo", especialidad: "Clínica Médica", consulta:"$ 40000", obraSocial:"Oser", matricula: "482365", foto: "./fotos/3.jpg" }
+    { nombre: "Dr. Juan Pérez", especialidad: "Cardiología",consulta:"$ 40000", obraSocial: "osde", matricula: "256985", foto: "../fotos/1.jpg" },
+    { nombre: "Dr. Manuel Aboy", especialidad: "Traumatología", consulta:"$ 40000", obraSocial: "Pami", matricula: "254796", foto: "../fotos/2.jpg" },
+    { nombre: "Dr. Osvaldo Rizzo", especialidad: "Clínica Médica", consulta:"$ 40000", obraSocial:"Oser", matricula: "482365", foto: "../fotos/3.jpg" }
   ];
 
   function cargarMedicos() {
